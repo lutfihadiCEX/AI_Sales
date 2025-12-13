471195aaf9cd788ea4570f9d58a787e4d282136c
lutfihadiCEX <lutfihadij@gmail.com>
2025-12-13 22:25:17 +0800

    Scripts for direct predict

---
+++ src/ml/predict.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import joblib
+import pandas as pd
+from src.data.preprocess import preprocess_sales_data
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+MODELS_DIR = PROJECT_ROOT / "models"
+
+def load_models():
+    """Load trained models/encoders"""
+    lr_model = joblib.load(MODELS_DIR / "linear_regression_model.pkl")
+    rf_model = joblib.load(MODELS_DIR / "random_forest_model.pkl")
+    encoders = joblib.load(MODELS_DIR / "encoders.pkl")
+
+    return lr_model, rf_model, encoders
+
+def run_prediction(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Prediction on new datasets. Returns in dataframe form
+
+    """
+    df_processed, _ = preprocess_sales_data(df)
+
+    drop_cols = [
+        "order_id",
+        "order_date",
+        "total_sales",
+        "is_anomaly",
+        "product_name",
+    ]
+
+    X = df_processed.drop(columns=[c for c in drop_cols if c in df_processed.columns])
+
+    lr_model, rf_model, _ = load_models()
+
+    df_processed["lr_predicted_sales"] = lr_model.predict(X)
+    df_processed["rf_predicted_sales"] = rf_model.predict(X)
+
+    return df_processed
+
+if __name__ == "__main__":
+    from src.data.load_data import load_sales_data
+
+    raw_df = load_sales_data()
+    predictions = run_prediction(raw_df)
+
+    print(predictions[
+        ["order_id", "total_sales", "lr_predicted_sales", "rf_predicted_sales", "is_anomaly"]
+    ].head())
