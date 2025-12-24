820b257f2ecbf0785908b4af9bcd1b30aa398213
lutfihadiCEX <lutfihadij@gmail.com>
2025-12-24 21:46:07 +0800

    Added XGB and removed deprecated lib syntax

--- src/ml/sales_model.py
+++ src/ml/sales_model.py
@@ -3,7 +3,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import root_mean_squared_error, r2_score
+from xgboost import XGBRegressor
 import joblib
 from src.data.load_data import load_sales_data
 from src.data.preprocess import preprocess_sales_data
@@ -31,11 +32,21 @@ def train_models():
     rf_model.fit(X_train, y_train)
     y_pred_rf = rf_model.predict(X_test)
 
+    # XGB 
+    xgb_model = XGBRegressor(n_estimators=300, learning_rate=0.05, max_depth=6, subsample=0.8, colsample_bytree=0.8,
+                             objective="reg:squarederror", random_state=42)
+    xgb_model.fit(X_train, y_train)
+    y_pred_xgb = xgb_model.predict(X_test)
+
     # Simple eval
-    print("Linear Regression RMSE:", mean_squared_error(y_test, y_pred_lr, squared=False))
+    print("Linear Regression RMSE:", root_mean_squared_error(y_test, y_pred_lr))
     print("Linear Regression R2:", r2_score(y_test, y_pred_lr))
-    print("Random Forest RMSE:", mean_squared_error(y_test, y_pred_rf, squared=False))
+
+    print("Random Forest RMSE:", root_mean_squared_error(y_test, y_pred_rf))
     print("Random Forest R2:", r2_score(y_test, y_pred_rf))
+
+    print("\nXGBoost RMSE:", root_mean_squared_error(y_test, y_pred_xgb))
+    print("XGBoost R2:", r2_score(y_test, y_pred_xgb))
 
     models_path = Path(__file__).resolve().parents[2] / "models"
     models_path.mkdir(parents=True, exist_ok=True)
@@ -43,11 +54,12 @@ def train_models():
     # Save for use
     joblib.dump(lr_model, models_path / "linear_regression_model.pkl")
     joblib.dump(rf_model, models_path / "random_forest_model.pkl")
+    joblib.dump(xgb_model, models_path / "xgboost_model.pkl")
     joblib.dump(encoders, models_path / "encoders.pkl")
 
 
-    return lr_model, rf_model, encoders
+    return lr_model, rf_model, xgb_model, encoders
 
 
 if __name__ == "__main__":
-    lr, rf, encoders = train_models()
+    lr, rf, xgb, encoders = train_models()
